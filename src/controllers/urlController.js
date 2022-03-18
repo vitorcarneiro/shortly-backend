d8d506c3e4c31fe70ebf2f06c65286223406aac1
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-03-18 15:39:54 -0300

    feat: find a shortened url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,58 +2,77 @@ import bcrypt from 'bcrypt';
 import { connection } from '../database.js';
 
 export async function shortenUrl(req, res) {
-    const { url } = req.body;
-    const userId = res.locals.user.id;
+  const { url } = req.body;
+  const userId = res.locals.user.id;
 
-    try {
-      const existingLongUrl = await connection.query(`
-        SELECT * FROM "longUrls"
+  try {
+    const existingLongUrl = await connection.query(`
+      SELECT * FROM "longUrls"
+        WHERE "longUrl"=$1`, [url]);
+
+    let longUrlId = null;
+    if (existingLongUrl.rowCount > 0) {
+      longUrlId = existingLongUrl.rows[0].id;
+    
+    } else {
+      await connection.query(`
+        INSERT INTO 
+          "longUrls"("longUrl") 
+          VALUES ($1)
+        `, [url]);
+
+      const newLongUrl = await connection.query(`
+        SELECT id FROM "longUrls"
           WHERE "longUrl"=$1`, [url]);
 
-      let longUrlId = null;
-      if (existingLongUrl.rowCount > 0) {
-        longUrlId = existingLongUrl.rows[0].id;
-      
-      } else {
-        await connection.query(`
-          INSERT INTO 
-            "longUrls"("longUrl") 
-            VALUES ($1)
-          `, [url]);
-
-        const newLongUrl = await connection.query(`
-          SELECT id FROM "longUrls"
-            WHERE "longUrl"=$1`, [url]);
-
-        longUrlId = newLongUrl.rows[0].id;          
-      }
-
-      let shortUrl = '';
-      function generateShortUrl() {
-        shortUrl = Math.random().toString(36).substring(2, 7);
-        return;
-      }
-
-      generateShortUrl();
-      const existingShortUrl = await connection.query(`
-      SELECT * FROM "shortUrls"
-        WHERE "shortUrl"=$1`, [shortUrl]);
-        
-      if (existingShortUrl.rowCount > 0) {
-        generateShortUrl();
-      }
-      
-      await connection.query(`
-          INSERT INTO 
-            "shortUrls" ("shortUrl", "userId", "longUrlId") 
-            VALUES ($1, $2, $3)
-          `, [shortUrl, userId, longUrlId]);
-
-      res.status(201).send({
-        "url": shortUrl
-      });
-    } catch (error) {
-      console.log(error);
-      return res.sendStatus(500);
+      longUrlId = newLongUrl.rows[0].id;          
     }
+
+    let shortUrl = '';
+    function generateShortUrl() {
+      shortUrl = Math.random().toString(36).substring(2, 7);
+      return;
+    }
+
+    generateShortUrl();
+    const existingShortUrl = await connection.query(`
+    SELECT * FROM "shortUrls"
+      WHERE "shortUrl"=$1`, [shortUrl]);
+      
+    if (existingShortUrl.rowCount > 0) {
+      generateShortUrl();
+    }
+    
+    await connection.query(`
+        INSERT INTO 
+          "shortUrls" ("shortUrl", "userId", "longUrlId") 
+          VALUES ($1, $2, $3)
+        `, [shortUrl, userId, longUrlId]);
+
+    res.status(201).send({
+      "url": shortUrl
+    });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
   }
+}
+
+export async function findUrl(req, res) {
+  const {shortUrl} = req.params;
+  
+  try {
+    const existingShortUrl = await connection.query(`
+    SELECT s.id, s."shortUrl", l."longUrl" AS url
+    FROM "shortUrls" s
+    JOIN "longUrls" l ON s."longUrlId"=l.id
+      WHERE "shortUrl"=$1`, [shortUrl]);
+
+    if(existingShortUrl.rowCount === 0) { return res.sendStatus(404); }
+
+    res.send(existingShortUrl.rows[0]);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/urlController.js";
+import { shortenUrl, findUrl } from "../controllers/urlController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 import urlSchema from "../schemas/urlSchema.js";
 
 const urlRouter = Router();
 urlRouter.post('/urls/shorten', validateSchemaMiddleware(urlSchema), validateTokenMiddleware, shortenUrl);
+urlRouter.get('/urls/:shortUrl', findUrl);
 export default urlRouter;
