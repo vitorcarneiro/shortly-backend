4460308af03f653894311ba58c24cccb81d0c498
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-03-18 15:25:22 -0300

    feat: shorten url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -3,13 +3,55 @@ import { connection } from '../database.js';
 
 export async function shortenUrl(req, res) {
     const { url } = req.body;
-
-    const shortUrl = Math.random().toString(36).substring(2,7);
-
-
+    const userId = res.locals.user.id;
 
     try {
-      res.status(201).send(result);
+      const existingLongUrl = await connection.query(`
+        SELECT * FROM "longUrls"
+          WHERE "longUrl"=$1`, [url]);
+
+      let longUrlId = null;
+      if (existingLongUrl.rowCount > 0) {
+        longUrlId = existingLongUrl.rows[0].id;
+      
+      } else {
+        await connection.query(`
+          INSERT INTO 
+            "longUrls"("longUrl") 
+            VALUES ($1)
+          `, [url]);
+
+        const newLongUrl = await connection.query(`
+          SELECT id FROM "longUrls"
+            WHERE "longUrl"=$1`, [url]);
+
+        longUrlId = newLongUrl.rows[0].id;          
+      }
+
+      let shortUrl = '';
+      function generateShortUrl() {
+        shortUrl = Math.random().toString(36).substring(2, 7);
+        return;
+      }
+
+      generateShortUrl();
+      const existingShortUrl = await connection.query(`
+      SELECT * FROM "shortUrls"
+        WHERE "shortUrl"=$1`, [shortUrl]);
+        
+      if (existingShortUrl.rowCount > 0) {
+        generateShortUrl();
+      }
+      
+      await connection.query(`
+          INSERT INTO 
+            "shortUrls" ("shortUrl", "userId", "longUrlId") 
+            VALUES ($1, $2, $3)
+          `, [shortUrl, userId, longUrlId]);
+
+      res.status(201).send({
+        "url": shortUrl
+      });
     } catch (error) {
       console.log(error);
       return res.sendStatus(500);
